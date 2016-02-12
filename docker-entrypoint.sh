8231e80dc2b3d1096cb1922bad3c560408d2ab89
Pierre Bertet <bonjour@pierrebertet.net>
2016-02-12 13:34:34 +0000

    Prepare for OS X (still not working on OS X)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,15 +8,15 @@ fi
 BRANCH_PATH=$HOME/$(echo $UITK_BRANCH | md5sum | awk '{print $1}')
 
 if [ -d "$BRANCH_PATH" ]; then
-	echo "The branch already exist. Pulling changes..."
+	echo "   The branch already exist. Pulling changes…\n"
 	cd $BRANCH_PATH && bzr pull
 else
-	echo "The branch does not exist. Fetching it..."
+	echo "   The branch does not exist. Fetching it…\n"
 	bzr branch "$UITK_BRANCH" "$BRANCH_PATH"
 fi
 
 if [ $? -ne 0 ]; then
-	echo "Error. Exiting..."
+	echo "   An error occurred. Exiting."
 	exit 1
 fi
 
